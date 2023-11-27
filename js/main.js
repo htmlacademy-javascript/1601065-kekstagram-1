8955d64a6fc9ce84e3f45245ee859667debd180b
Zubareva Svetlana <se.berseneva@gmail.com~>
2023-11-27 18:45:25 +0300

    Убрала лишнее из файла

--- js/main.js
+++ js/main.js
@@ -4,16 +4,6 @@ const COMMENT_COUNT = 20;
 const LIKES_MIN = 15;
 const LIKES_MAX = 200;
 
-// const IDS = [
-//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-// ];
-
-const URLS = [
-  'photos/1.jpg', 'photos/2.jpg', 'photos/3.jpg', 'photos/4.jpg', 'photos/5.jpg', 'photos/6.jpg', 'photos/7.jpg',
-  'photos/8.jpg', 'photos/9.jpg', 'photos/10.jpg', 'photos/11.jpg', 'photos/12.jpg', 'photos/13.jpg', 'photos/14.jpg',
-  'photos/15.jpg', 'photos/16.jpg', 'photos/17.jpg', 'photos/18.jpg', 'photos/19.jpg', 'photos/20.jpg', 'photos/21.jpg',
-  'photos/22.jpg', 'photos/23.jpg', 'photos/24.jpg', 'photos/25.jpg',
-];
 
 const DESCRIPTIONS = [
   'Витя', 'Маша', 'Катя', 'Мища', 'Рита', 'Света', 'Никита', 'Гоша', 'Матвей', '1Лёша', '1Лариса', 'Артем', 'Дима',
